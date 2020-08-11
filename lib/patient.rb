922b93ac7a6b2e7c5da96179064c5e2c94793e87
Trevis McGill <trevismcgill25@gmail.com>
2020-08-11 16:14:54 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,3 +1,19 @@
 class Patient
+  attr_accessor :name
 
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+  
 end
