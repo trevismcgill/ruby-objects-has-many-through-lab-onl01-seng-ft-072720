58742b7f6373a582f77eb8b73b98e621e0eea51b
Trevis McGill <trevismcgill25@gmail.com>
2020-08-11 16:19:22 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -20,8 +20,8 @@ def appointments
 Appointment.all.select {|appointment| appointment.doctor == self}
 end
 
-def new_appointment
-
+def new_appointment(date, patient)
+Appointment.new(date, patient, self)
 end
 
 def patients
